2930f6e4ce70fce5dcb0c7e15f9cfc52217c8be2
l4u532 <k.langenheldt@gmail.com>
2017-07-01 14:34:08 +0200

    add legend

--- js/graph.js
+++ js/graph.js
@@ -150,3 +150,31 @@ function restart() {
     node.enter().insert("circle", ".cursor").attr("class", "node").attr("r", 5).call(force.drag);
     force.start();
 }
+
+var legendRectSize = 36;
+var legendSpacing = 8;
+
+var legend = d3.select('svg')
+    .append("g")
+    .selectAll("g")
+    .data(color.domain())
+    .enter()
+    .append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) {
+        var height = legendRectSize;
+        var x = 0;
+        var y = i * height;
+        return 'translate(' + x + ',' + y + ')';
+    });
+
+legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
